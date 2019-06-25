c8cea6730352b5e1ef97c4c48c07c68949c4cc28
yaqootturman <yaqoot.turman@gmail.com>
2019-06-25 14:14:06 +0300

    add to list

--- dom.js
+++ dom.js
@@ -1,28 +1,27 @@
-
 // part 2 linking it all together
 // The function here is called an iife,
 // it keeps everything inside hidden from the rest of our application
 (function() {
   // This is the dom node where we will keep our todo
-  var container = document.getElementById('todo-container');
-  var addTodoForm = document.getElementById('add-todo');
+  var container = document.getElementById("todo-container");
+  var addTodoForm = document.getElementById("add-todo");
 
   var state = [
-    { id: -3, description: 'first todo' },
-    { id: -2, description: 'second todo' },
-    { id: -1, description: 'third todo' },
+    { id: -3, description: "first todo" },
+    { id: -2, description: "second todo" },
+    { id: -1, description: "third todo" }
   ]; // this is our initial todoList
 
   // This function takes a todo, it returns the DOM node representing that todo
   var createTodoNode = function(todo) {
-    var todoNode = document.createElement('li');
+    var todoNode = document.createElement("li");
     // you will need to use addEventListener
 
     // add span holding description
 
     // this adds the delete button
-    var deleteButtonNode = document.createElement('button');
-    deleteButtonNode.addEventListener('click', function(event) {
+    var deleteButtonNode = document.createElement("button");
+    deleteButtonNode.addEventListener("click", function(event) {
       var newState = todoFunctions.deleteTodo(state, todo.id);
       update(newState);
     });
@@ -37,12 +36,12 @@
 
   // bind create todo form
   if (addTodoForm) {
-    addTodoForm.addEventListener('submit', function(event) {
+    addTodoForm.addEventListener("submit", function(event) {
       // https://developer.mozilla.org/en-US/docs/Web/Events/submit
       // what does event.preventDefault do?
       // what is inside event.target?
 
-      var description = '?'; // event.target ....
+      var description = "?"; // event.target ....
 
       // hint: todoFunctions.addTodo
       var newState = []; // ?? change this!
@@ -58,7 +57,7 @@
 
   // you do not need to change this function
   var renderState = function(state) {
-    var todoListNode = document.createElement('ul');
+    var todoListNode = document.createElement("ul");
 
     state.forEach(function(todo) {
       todoListNode.appendChild(createTodoNode(todo));
@@ -70,61 +69,61 @@
 
   if (container) renderState(state);
 })();
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Skip to content
- 
-Search or jump to…
+// © 2019 GitHub, Inc.
+// Terms
+// Privacy
+// Security
+// Status
+// Help
+// Contact GitHub
+// Pricing
+// API
+// Training
+// Blog
+// About
+// Skip to content
 
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@RandInaim 
-0
-0 69 fack2/master-reference
-forked from foundersandcoders/master-reference
- Code  Issues 2  Pull requests 1  Projects 0  Security  Insights
-master-reference/coursebook/week-2/project/skeleton/dom.js
-@finnhodgkin finnhodgkin renames project folder and old stopwatch project
-bb49293 on Jun 25, 2017
-72 lines (56 sloc)  2.06 KB
-  
+// Search or jump to…
+
+// Pull requests
+// Issues
+// Marketplace
+// Explore
+
+// @RandInaim
+// 0
+// 0 69 fack2/master-reference
+// forked from foundersandcoders/master-reference
+//  Code  Issues 2  Pull requests 1  Projects 0  Security  Insights
+// master-reference/coursebook/week-2/project/skeleton/dom.js
+// @finnhodgkin finnhodgkin renames project folder and old stopwatch project
+// bb49293 on Jun 25, 2017
+// 72 lines (56 sloc)  2.06 KB
+
 // part 2 linking it all together
 // The function here is called an iife,
 // it keeps everything inside hidden from the rest of our application
 (function() {
   // This is the dom node where we will keep our todo
-  var container = document.getElementById('todo-container');
-  var addTodoForm = document.getElementById('add-todo');
+  var container = document.getElementById("todo-container");
+  var addTodoForm = document.getElementById("add-todo");
 
   var state = [
-    { id: -3, description: 'first todo' },
-    { id: -2, description: 'second todo' },
-    { id: -1, description: 'third todo' },
+    { id: -3, description: "first todo" },
+    { id: -2, description: "second todo" },
+    { id: -1, description: "third todo" }
   ]; // this is our initial todoList
 
   // This function takes a todo, it returns the DOM node representing that todo
   var createTodoNode = function(todo) {
-    var todoNode = document.createElement('li');
+    var todoNode = document.createElement("li");
     // you will need to use addEventListener
 
     // add span holding description
 
     // this adds the delete button
-    var deleteButtonNode = document.createElement('button');
-    deleteButtonNode.addEventListener('click', function(event) {
+    var deleteButtonNode = document.createElement("button");
+    deleteButtonNode.addEventListener("click", function(event) {
       var newState = todoFunctions.deleteTodo(state, todo.id);
       update(newState);
     });
@@ -139,12 +138,12 @@ bb49293 on Jun 25, 2017
 
   // bind create todo form
   if (addTodoForm) {
-    addTodoForm.addEventListener('submit', function(event) {
+    addTodoForm.addEventListener("submit", function(event) {
       // https://developer.mozilla.org/en-US/docs/Web/Events/submit
       // what does event.preventDefault do?
       // what is inside event.target?
 
-      var description = '?'; // event.target ....
+      var description = "?"; // event.target ....
 
       // hint: todoFunctions.addTodo
       var newState = []; // ?? change this!
@@ -160,7 +159,7 @@ bb49293 on Jun 25, 2017
 
   // you do not need to change this function
   var renderState = function(state) {
-    var todoListNode = document.createElement('ul');
+    var todoListNode = document.createElement("ul");
 
     state.forEach(function(todo) {
       todoListNode.appendChild(createTodoNode(todo));
