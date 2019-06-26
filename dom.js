b8cf81f5553495d1fe059e06a40df35d78b853a5
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-06-26 16:13:25 +0300

    added mark to do function and dom

--- dom.js
+++ dom.js
@@ -4,119 +4,123 @@
 // The function here is called an iife,
 // it keeps everything inside hidden from the rest of our application
 (function() {
-	// This is the dom node where we will keep our todo
-	var container = document.getElementById("todo-container");
-	var addTodoForm = document.getElementById("add-todo");
+  // This is the dom node where we will keep our todo
+  var container = document.getElementById("todo-container");
+  var addTodoForm = document.getElementById("add-todo");
 
-	var submit = document.getElementById("submitButton");
+  var submit = document.getElementById("submitButton");
 
-	submit.addEventListener("click", function(event) {
-		event.preventDefault();
-		var text = document.getElementById("inputText");
+  submit.addEventListener("click", function(event) {
+    event.preventDefault();
+    var text = document.getElementById("inputText");
 
-		var myObject = {
-			id: todoFunctions.generateId(),
-			description: text.value,
-			status: false
-		};
-		var newState = todoFunctions.addTodo(state, myObject); //the new array
-		update(newState);
-		createTodoNode(myObject);
-	});
+    var myObject = {
+      id: todoFunctions.generateId(),
+      description: text.value,
+      status: false
+    };
+    var newState = todoFunctions.addTodo(state, myObject); //the new array
+    update(newState);
+    createTodoNode(myObject);
+  });
 
-	var state = [{ id: -3, description: "first todo", status: false }]; // this is our initial todoList
+  var state = [{ id: -3, description: "first todo", status: false }]; // this is our initial todoList
+  var sortButton = document.getElementById("sort");
+  sortButton.addEventListener("click", function() {
+    var sortFunction = (a, b) => (a.status > b.status ? 1 : -1);
+    var sorted = todoFunctions.sortTodos(state, sortFunction);
+    update(sorted);
+  });
 
-	var sortButton = document.getElementById("sort");
-	sortButton.addEventListener("click", function() {
-		var sortFunction = (a, b) => (a.done > b.done ? 1 : -1);
-		var sorted = todoFunctions.sortTodos(state, sortFunction);
-		update(sorted);
-	});
+  // This function takes a todo, it returns the DOM node representing that todo
+  var createTodoNode = function(todo) {
+    var todoNode = document.createElement("li");
 
-	// This function takes a todo, it returns the DOM node representing that todo
-	var createTodoNode = function(todo) {
-		var todoNode = document.createElement("li");
+    //add the checkbox
+    var checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.checked = todo.status;
+    todoNode.appendChild(checkbox);
+    checkbox.addEventListener("change", function(event) {
+      const newstate = todoFunctions.markTodo(state, todo.id);
+      update(newstate);
+    });
 
-		//add the checkbox
-		var checkbox = document.createElement("input");
-		checkbox.type = "checkbox";
-		todoNode.appendChild(checkbox);
+    // text of item list
+    var textElement = document.createElement("p");
+    textElement.innerText = todo.description;
+    textElement.contentEditable = "false";
+    todoNode.appendChild(textElement);
 
-		// text of item list
-		var textElement = document.createElement("p");
-		textElement.innerText = todo.description;
-		textElement.contentEditable = "false";
-		todoNode.appendChild(textElement);
+    // edit button
+    var editButtonNode = document.createElement("button");
+    var editIcon = document.createElement("i");
+    editIcon.innerHTML = '<i class="material-icons">&#xe22b;</i>';
+    editButtonNode.appendChild(editIcon);
+    todoNode.appendChild(editButtonNode);
+    var count = 1;
+    editButtonNode.addEventListener("click", function(event) {
+      if (count % 2 != 0) {
+        textElement.contentEditable = true;
+      } else {
+        textElement.contentEditable = false;
+      }
+      count++;
+    });
 
-		// edit button
-		var editButtonNode = document.createElement("button");
-		var editIcon = document.createElement("i");
-		editIcon.innerHTML = '<i class="material-icons">&#xe22b;</i>';
-		editButtonNode.appendChild(editIcon);
-		todoNode.appendChild(editButtonNode);
-		var count = 1;
-		editButtonNode.addEventListener("click", function(event) {
-			if (count % 2 != 0) {
-				textElement.contentEditable = true;
-			} else {
-				textElement.contentEditable = false;
-			}
-			count++;
-		});
+    // add span holding descriptiondeleteTodo
+    // this adds the delete button
+    var deleteButtonNode = document.createElement("button");
+    var deleteIcon = document.createElement("i");
+    deleteIcon.innerHTML = '<i class="material-icons">&#10007;</i>';
+    deleteButtonNode.appendChild(deleteIcon);
 
-		// add span holding descriptiondeleteTodo
-		// this adds the delete button
-		var deleteButtonNode = document.createElement("button");
-		var deleteIcon = document.createElement("i");
-		deleteIcon.innerHTML = '<i class="material-icons">&#10007;</i>';
-		deleteButtonNode.appendChild(deleteIcon);
+    deleteButtonNode.style.background = "red";
+    deleteButtonNode.addEventListener("click", function(event) {
+      var newState = todoFunctions.deleteTodo(state, todo.id);
+      update(newState);
+    });
+    todoNode.appendChild(deleteButtonNode);
 
-		deleteButtonNode.style.background = "red";
-		deleteButtonNode.addEventListener("click", function(event) {
-			var newState = todoFunctions.deleteTodo(state, todo.id);
-			update(newState);
-		});
-		todoNode.appendChild(deleteButtonNode);
+    // add markTodo button
+    // var markTodo = getElementById();
+    // add classes for css
 
-		// add markTodo button
+    return todoNode;
+  };
 
-		// add classes for css
+  // bind create todo form
+  if (addTodoForm) {
+    addTodoForm.addEventListener("submit", function(event) {
+      // https://developer.mozilla.org/en-US/docs/Web/Events/submit
+      // what does event.preventDefault do?
+      // what is inside event.target?
 
-		return todoNode;
-	};
+      var description = "?"; // event.target ....
 
-	// bind create todo form
-	if (addTodoForm) {
-		addTodoForm.addEventListener("submit", function(event) {
-			// https://developer.mozilla.org/en-US/docs/Web/Events/submit
-			// what does event.preventDefault do?
-			// what is inside event.target?
+      // hint: todoFunctions.addTodo
+      var newState = []; // ?? change this!
+      update(newState);
+    });
+  }
 
-			var description = "?"; // event.target ....
+  // you should not need to change this function
+  var update = function(newState) {
+    state = newState;
+    renderState(state);
+  };
 
-			// hint: todoFunctions.addTodo
-			var newState = []; // ?? change this!
-			update(newState);
-		});
-	}
+  // you do not need to change this function
+  var renderState = function(state) {
+    var todoListNode = document.createElement("ul");
 
-	// you should not need to change this function
-	var update = function(newState) {
-		state = newState;
-		renderState(state);
-	};
+    state.forEach(function(todo) {
+      todoListNode.appendChild(createTodoNode(todo));
+    });
 
-	// you do not need to change this function
-	var renderState = function(state) {
-		var todoListNode = document.createElement("ul");
+    // you may want to add a class for css
+    container.replaceChild(todoListNode, container.firstChild);
+  };
 
-		state.forEach(function(todo) {
-			todoListNode.appendChild(createTodoNode(todo));
-		});
-
-		// you may want to add a class for css
-		container.replaceChild(todoListNode, container.firstChild);
-	};
-
-	if (container) renderState(state);
+  if (container) renderState(state);
 })();
